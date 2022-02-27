f3808efb19aad4e6c2d88607fc2ed740cb2be8ad
kj <jun111haha@gmail.com>
2022-02-27 16:06:32 +0900

    22.02.27

---
+++ src/BaekJoon/X보다작은수.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class X보다작은수 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int [] arr = new int[a];
+
+        for(int i=0; i<a; i++){
+            arr[i] = scanner.nextInt();
+        }
+
+        for(int i=0; i<a; i++){
+            if(arr[i] < b){
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
